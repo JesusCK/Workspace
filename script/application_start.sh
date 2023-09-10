52a7428eba0cfbd080373bc4b747bc94970fe28e
JesusCK <142179535+JesusCK@users.noreply.github.com>
2023-09-10 16:04:13 -0500

    Create application_start.sh

---
+++ script/application_start.sh
@@ -0,0 +1,10 @@
+source /home/ubuntu/Workspace/.venv/bin/activate
+
+# Cambia al directorio de tu aplicación Flask.
+cd /home/ubuntu/Workspace
+
+# Instala las dependencias de tu aplicación Flask (por ejemplo, utilizando pip).
+
+
+# Inicia la aplicación Flask.
+python3 APP.py  # Ajusta el nombre de tu archivo de aplicación Flask.
